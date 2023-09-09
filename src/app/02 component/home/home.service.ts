1beab82c71c98da194960ebfa62c81461c074a77
natarajanmk <natarajanmk@gmail.com>
2023-09-09 11:32:25 +0530

    1 file modified

--- src/app/02 component/home/home.service.ts
+++ src/app/02 component/home/home.service.ts
@@ -4,25 +4,21 @@ import {
   Resolve,
   RouterStateSnapshot,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Subject, BehaviorSubject, Observable } from 'rxjs';
 import { LoginInfo } from '../admin/login.auth.demo.service';
 import { LoginAuthService } from '../admin/login.auth.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class HomeService implements Resolve<LoginInfo[]> {
   constructor(private loginAuthService: LoginAuthService) {}
 
-  testLoginInfo: LoginInfo[] = [];
+  _loginUserInfo = new BehaviorSubject<LoginInfo[] | undefined>(undefined);
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<LoginInfo[]> | Promise<LoginInfo[]> | LoginInfo[] {
-    let result = this.loginAuthService.getAllLoginUser();
-    for (let i = 0; i < 10; i++) {
-      this.testLoginInfo.push(result);
-    }
-    //return this.loginAuthService.getAllLoginUser();
-    return this.testLoginInfo;
+    return this.loginAuthService.getAllLoginUser();
   }
 }
